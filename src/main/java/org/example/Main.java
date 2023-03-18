243b0d14ac1eadf4e60f41ca6bdb695a38cc0706
Martin Jäger <martinja@edu.aau.at>
2023-03-18 18:23:32 +0100

    First basic Implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,37 @@
 package org.example;
+import com.sun.net.httpserver.Headers;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-//import com.deepl.DeepLClient;
-//import com.deepl.util.Language;
+import com.deepl.api.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String url = "https://de.wikipedia.org/";
+    static Translator translator;
+    public static void main(String[] args) throws IOException, DeepLException, InterruptedException {
+        String url = args[0];
+        int depth = Integer.parseInt(args[1]);
+        String targetLanguage = args[2];
+        String sourceLanguage;
+        //System.out.println(language);
+
+        String authKey = "56a1abfc-d443-0e69-8963-101833b4014e:fx";  // Replace with your key
+        translator = new Translator(authKey);
+        //TextResult result = translator.translateText("Hello, world!", null, language);
+        //System.out.println(result.getText());
+
         Document doc = Jsoup.connect(url).get();
-        String text = doc.select("header").text();
-        System.out.println(text);
+
+        Elements el = doc.select("h1");
+        //sourceLanguage =
+        TextResult result;
+
+        for(Element e : el){
+            result = translator.translateText(e.toString(), null, targetLanguage);
+            //System.out.printf("%s\n",e);
+            System.out.println(result.getText());
+        }
     }
 }
