341f9f7d95abc9709b4837e9358fcd5c5b4e6613
Martin Jäger <martinja@edu.aau.at>
2023-03-18 19:10:55 +0100

    Commit

--- src/main/java/at/gr6/crawler/Main.java
+++ src/main/java/at/gr6/crawler/Main.java
@@ -15,24 +15,18 @@ public class Main {
         String url = args[0];
         int depth = Integer.parseInt(args[1]);
         String targetLanguage = args[2];
-
         String sourceLanguage ="";
-        //System.out.println(language);
 
-        String authKey = "56a1abfc-d443-0e69-8963-101833b4014e:fx";  // Replace with your key
+        String authKey = "56a1abfc-d443-0e69-8963-101833b4014e:fx";
         translator = new Translator(authKey);
-        //TextResult result = translator.translateText("Hello, world!", null, language);
-        //System.out.println(result.getText());
 
         Document doc = Jsoup.connect(url).get();
 
-
-        Evaluator ev = new Evaluator.Attribute("h");
-        Elements el = doc.select("h1");
+        Elements el = doc.select("h1,h2,h3,h4,h5,h6");
         TextResult result;
 
         for(Element e : el){
-            result = translator.translateText(e.toString(), null, targetLanguage);
+            result = translator.translateText(e.text(), null, targetLanguage);
             sourceLanguage = result.getDetectedSourceLanguage();
             System.out.println(result.getText());
         }
