5146511e7515704092c5653c8ad973304badc0c9
Martin Jäger <martinja@edu.aau.at>
2023-03-19 17:12:11 +0100

    Main Implementation

--- report.md
+++ report.md
@@ -1,1 +1,5 @@
-asdasdasdasdasdasdasd
+input: <a>https://orf.at/stories/3309371/</a>
+<br>depth: 1
+<br>source language: Kärntnerisch
+<br>target language: German
+<br>summary:
--- src/main/java/at/gr6/crawler/Main.java
+++ src/main/java/at/gr6/crawler/Main.java
@@ -8,7 +8,9 @@ import org.jsoup.select.Elements;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,38 +27,76 @@ public class Main {
     static HashMap<String, Integer> languageStatistics = new HashMap<String, Integer>();
     static boolean translate = false;
     static Link link;
+    static Page page;
+    static FileWriter fw;
 
     public static void main(String[] args) throws IOException, DeepLException, InterruptedException {
-        //url = /*"http://www.broken-404.com"*/args[0];
-        /*depth = Integer.parseInt(args[1]);
+        setup();
+        url = /*"http://www.broken-404.com"*/args[0];
+        depth = Integer.parseInt(args[1]);
         targetLanguage = args[2];
         link = new Link(url, 0);
 
         String authKey = "56a1abfc-d443-0e69-8963-101833b4014e:fx";
         translator = new Translator(authKey);
         System.out.println("--------------------PAGE-START--------------------");
+
         readPage(link);
 
         sourceLanguage = getFullLanguage(getLanguage().toUpperCase());
         System.out.println("Source language: " + sourceLanguage);
-        System.out.println("Target language: " + getFullLanguage(targetLanguage.toUpperCase()));*/
+        System.out.println("Target language: " + getFullLanguage(targetLanguage.toUpperCase()));
 
         writeFile();
     }
 
+    private static void setup() {
+        try {
+            fw = new FileWriter("./report.md");
+        } catch (Exception e) {
+        }
+    }
+
+
     private static void writeFile() {
         try {
-            FileWriter fw = new FileWriter("./report.md");
-            fw.write("input: <a>"+url+"</a>\n");
-
-            System.out.println("AAAAAAAAA");
+            fw.write("input: <a>" + url + "</a>\n");
+            fw.write("<br>depth: " + depth + "\n");
+            fw.write("<br>source language: " + sourceLanguage + "\n");
+            fw.write("<br>target language: " + getFullLanguage(targetLanguage.toUpperCase()) + "\n");
+            fw.write("<br>summary:\n");
             fw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void readPage(Link link) throws IOException, DeepLException, InterruptedException {
+    private static void writeHeader(String headers) {
+        //System.out.println(headers);
+        try {
+            fw.write(headers);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeLink(String link) {
+        try {
+            fw.write(link);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String setCorrectIndentation(int depth) {
+        String indents = " ";
+        for (int i = 0; i < depth; i++) {
+            indents += "-";
+        }
+        return (indents += ">");
+    }
+
+    private static void readPage(Link link) {
         try {
             doc = Jsoup.connect(link.getUrl()).get();
 
@@ -64,21 +104,33 @@ public class Main {
             Elements headers = doc.select("h1,h2,h3,h4,h5,h6"); //All Headers
             Elements links = doc.select("a[href^=http]");       //All Links with prefix "http"
             Link link1;
-            Page page1 = new Page();
-
+            page = new Page();
+            String headerString = "";
 
             for (Element h : headers) {     //Headers
-                page1.getHeader().add(h);
-                translateHeader(h);
+                Element res = translateHeader(h);
+                //Element res = new Element(result.getText());
+                page.getHeader().add(res);
+                //System.out.println(res.tag());
+                for (int i = Integer.parseInt(h.tagName().charAt(1) + ""); i > 0; i--)
+                    headerString += "#";
+                headerString += setCorrectIndentation(currentDepth);
+                headerString += (" " + res.tag() + "\n");
             }
+            writeHeader(headerString);  //Write Headers
+
 
             for (Element e : links) {
                 String url1 = e.attr("href");
-
                 link1 = new Link(url1, currentDepth + 1);    //Next Page is old depth+1
-                page1.getLinks().add(link1);
-                depthSymbols(currentDepth + 1);
-                System.out.println("link to: " + link1);
+                page.getLinks().add(link1);
+                //depthSymbols(currentDepth + 1);
+                String linkStr = "";
+                linkStr += setCorrectIndentation(currentDepth);
+                linkStr += (" " + url1 + "\n");
+                writeLink(linkStr);   //Write Links
+
+                //System.out.println("link to: " + link1);
 
                 if (currentDepth < depth) {
                     readPage(link1);
@@ -98,24 +150,22 @@ public class Main {
         System.out.print(">");
     }
 
-    private static void translateHeader(Element e) throws DeepLException, InterruptedException {
+    private static Element translateHeader(Element e) throws DeepLException, InterruptedException {
+        TextResult result;
         if (translate)
             result = translator.translateText(e.text(), null, targetLanguage);
         else
             result = new TextResult(e.text(), "");
 
-        for (int i = Integer.parseInt(e.tagName().charAt(1) + ""); i > 0; i--) {
-            System.out.print("#");
-        }
         sourceLanguage = result.getDetectedSourceLanguage();
-        //System.out.println("Lang: "+sourceLanguage);
 
         if (!languageStatistics.containsKey(sourceLanguage))     //For language statistics
             languageStatistics.put(sourceLanguage, 1);
         else
             languageStatistics.put(sourceLanguage, languageStatistics.get(sourceLanguage) + 1);
-        System.out.println(" " + result.getText());
+        //System.out.println(" " + result.getText()); --------------------------------------------------------------
 
+        return new Element(result.getText());
     }
 
     private static String getLanguage() {
