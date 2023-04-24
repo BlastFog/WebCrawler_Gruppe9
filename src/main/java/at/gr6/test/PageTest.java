3c6c3ffdbdc851db2e3302d56bf28ef3d65b9dec
Martin Jäger <martinja@edu.aau.at>
2023-04-24 16:05:54 +0200

    Refactored the PageTest

--- src/main/java/at/gr6/test/PageTest.java
+++ src/main/java/at/gr6/test/PageTest.java
@@ -20,12 +20,12 @@ class PageTest {
 
     @BeforeEach
     void setUp() {
-        page = new Page(url,depth);
-
+        page = new Page(url, depth);
     }
+
     @Test
-    void testConstructor(){
-        Page page1 = new Page(url,depth);
+    void testConstructor() {
+        Page page1 = new Page(url, depth);
         assertTrue(page1.equals(page));
         Page page2 = new Page("wrong url", 2);
         assertFalse(page2.equals(page));
@@ -34,17 +34,13 @@ class PageTest {
     @Test
     void testHeaderStringList() {
         ArrayList<String> headerList = new ArrayList<>();
-        for (int i =0;i<10; i++){
-            headerList.add(header+i);
-        }
+        for (int i = 0; i < 10; i++)
+            headerList.add(header + i);
         page.setHeaderStringList(headerList);
 
-        int index = 0;
         ArrayList<String> actualHeaderStringList = page.getHeaderStringList();
-        for(int i = 0;i<10;i++){
-            assertEquals(headerList.get(i),actualHeaderStringList.get(i));
-            index++;
-        }
+        for (int i = 0; i < 10; i++)
+            assertEquals(headerList.get(i), actualHeaderStringList.get(i));
     }
 
     @Test
@@ -57,27 +53,18 @@ class PageTest {
         page.setBroken(true);
         assertTrue(page.isBroken());
     }
-  /*  @Test
-    void testGetDepth() {
-        assertEquals(page.getDepth(),1);
-    }
 
-
-    @Test
-    void testGetSubPage() {
-        assertNotNull(page.getSubPage());
-    }*/
     @Test
     void testSetSubPages() {
         ArrayList<String> linkList = new ArrayList<String>();
-        for(int i=0;i<10;i++){
-            linkList.add(url+i);
+        for (int i = 0; i < 10; i++) {
+            linkList.add(url + i);
         }
         page.setSubPages(linkList);
         int index = 0;
-        for (Page subPage:page.getSubPage()) {
-            assertEquals(linkList.get(index),subPage.getUrl());
-            assertEquals(depth+1,subPage.getDepth());
+        for (Page subPage : page.getSubPage()) {
+            assertEquals(linkList.get(index), subPage.getUrl());
+            assertEquals(depth + 1, subPage.getDepth());
             index++;
         }
     }
@@ -85,64 +72,54 @@ class PageTest {
     @Test
     void testGetformattedPage() {
         ArrayList<String> headerList = new ArrayList<>();
-        for (int i =0;i<5; i++){
-            headerList.add(header+i);
-        }
+        for (int i = 0; i < 5; i++)
+            headerList.add(header + i);
         page.setHeaderStringList(headerList);
 
         ArrayList<String> linkList = new ArrayList<String>();
-        for(int i=0;i<5;i++){
-            linkList.add(url+i);
-        }
+        for (int i = 0; i < 5; i++)
+            linkList.add(url + i);
         page.setSubPages(linkList);
         page.getSubPage().get(4).setBroken(true);
         String expected = generateExpectedString(depth);
 
-        assertEquals(expected,page.getformattedPage());
-
-        }
-
-
-
-
-
-    private String generateExpectedString(int depth){
-        String expected = "";
-        for(int i = 0;i<5;i++){
-            expected+=fixMarkdownFormat(header+i,depth)+"\n";
-        }
-        expected+="\n";
-        for(int i = 0;i<4;i++){
-            expected+="<br> "+getIndentation(depth)+"link to <a>"+url+i+"</a>"+"\n";
-        }
-        expected+="<br> "+getIndentation(depth)+"broken link <a>"+url+4+"</a>"+"\n";
-        return expected;
-
+        assertEquals(expected, page.getformattedPage());
     }
-    private String fixMarkdownFormat(String header,int depth){
-        String headerGrade= "";
+
+
+    private String generateExpectedString(int depth) {
+        String expected = "";
+        for (int i = 0; i < 5; i++)
+            expected += fixMarkdownFormat(header + i, depth) + "\n";
+        expected += "\n";
+        for (int i = 0; i < 4; i++)
+            expected += "<br> " + getIndentation(depth) + "link to <a>" + url + i + "</a>" + "\n";
+        expected += "<br> " + getIndentation(depth) + "broken link <a>" + url + 4 + "</a>" + "\n";
+        return expected;
+    }
+
+    private String fixMarkdownFormat(String header, int depth) {
+        String headerGrade = "";
         String headerString = "";
-        for(int i = 0;i<header.length();i++){
-            if(header.charAt(i)!='#'){
-                headerGrade  = header.substring(0,i);
+        for (int i = 0; i < header.length(); i++) {
+            if (header.charAt(i) != '#') {
+                headerGrade = header.substring(0, i);
                 headerString = header.substring(i);
                 break;
             }
 
         }
-        String result = headerGrade+" "+getIndentation(depth)+headerString;
+        String result = headerGrade + " " + getIndentation(depth) + headerString;
         return result;
     }
 
-    private String getIndentation(int depth){
-        String result ="";
-        for (int i=0;i<depth;i++){
-            result+="-";
-        }
-        result+=">";
+    private String getIndentation(int depth) {
+        String result = "";
+        for (int i = 0; i < depth; i++)
+            result += "-";
+        result += ">";
         return result;
     }
-
 
 
 }
