d3bdaa8735af4d71af5ba20413078ee241fe5636
BlastFog <martinjger@ymail.com>
2023-04-24 12:47:34 +0200

    Added a test for the main method

--- src/main/java/at/gr6/test/MainTest.java
+++ src/main/java/at/gr6/test/MainTest.java
@@ -1,5 +1,19 @@
 package at.gr6.test;
 
+import at.gr6.crawler.Main;
+import at.gr6.crawler.Page;
+import org.junit.Test;
+
+import static at.gr6.crawler.Main.main;
+import static org.mockito.Mockito.*;
+
+
 public class MainTest {
 
+    @Test
+    public void testMain(){
+        mockStatic(Page.class);
+        String[] args = {"https://example.com","1","en-GB"};
+        main(args);
+    }
 }
